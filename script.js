6d7bece42bafba3acb1fe5b138b6fc61cff403ec
sohaib <ahmadsohaib1216@gmail.com>
2016-06-04 13:34:01 +0100

    upload1

--- script.js
+++ script.js
@@ -1,33 +1,58 @@
-jQuery(document).ready(function(){
-    // hiding experience content
-    jQuery("#experience ul").hide();
-    jQuery("#experience h4").click(function(){
-    jQuery(this).next("ul").slideToggle();    
-    });
-    jQuery("#experience h4").addClass(".responsibilities");
-    // finish of experience content
+    jQuery(document).ready(function(){
+        // variables for sliding
+        var windowHeight = jQuery(window).height();    
+          var windowScrollPosTop = jQuery(window).scrollTop();   
+          var windowScrollPosBottom = windowHeight+windowScrollPosTop;
+        
+        
+        
+        // hiding experience content
+        jQuery("#experience ul").hide();
+        jQuery("#experience h4").click(function(){
+        jQuery(this).next("ul").slideToggle();    
+        });
+        jQuery("#experience h4").addClass(".responsibilities");
+        // finish of experience content
+
     
-    //scrolling
-//    jQuery(function(){
-//      jQuery('a[href^="#"]').click(function(e){
-//            var target = jQuery(this).attr('href');
-//            var strip= target.slice(1);
-//            var anchor= jQuery("a[id='" + strip + "']");
-//            e.preventDefault();
-//            $('html,body').animate({
-//              scrollTop: anchor.offset().top;  
-//            },2000);
-//                    
-//                                 
-//                                 
-//    });
-//        
-//        
-//           
-//    });
-    //scroll close
-    
-    
-    
-    
-});// final close
+
+        //sliding effec     //jQuery(".photo1").css("opacity,0");
+        jQuery.fn.revealOnScroll=function(){
+        return this.each(function(){
+            var objectOffSet = jQuery(this).offset();
+        var objectOffSetTop = objectOffSet.top;
+
+            if(!jQuery(this).hasClass("animation-done")){
+                if(windowScrollPosBottom > objectOffSetTop){
+                jQuery(this).animate({"opacity":1},3000).addClass("animation-done");
+            }
+
+            }
+            
+        });    
+            
+            
+        }// end of function
+        
+        jQuery(window).scroll(function(){
+
+          windowHeight = jQuery(window).height();    
+          windowScrollPosTop = jQuery(window).scrollTop();   
+          windowScrollPosBottom = windowHeight+windowScrollPosTop;
+            
+            jQuery(".exp img").revealOnScroll();
+            jQuery("#imagecircle").revealOnScroll();
+        
+
+        });
+
+
+
+
+
+
+
+
+
+
+    });// final close
